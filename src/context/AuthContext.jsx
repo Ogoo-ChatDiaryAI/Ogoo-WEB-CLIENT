d3a2164462c81378f952ad713aa26c3496f58fee
ocahs9 <piorag99@gmail.com>
2024-10-09 03:00:47 +0900

    chore: /로 이동하면 로그인 여부 사라지는 이유 주석

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useState } from "react";
 
 const AuthContext = createContext();
 
+//얘도 결국 새로고침, or 브라우저에 직접 입력을 하면 전역 상태라서 날라감. 로그인 상태 유지하고 싶으면 localStorage 이용해야함
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
