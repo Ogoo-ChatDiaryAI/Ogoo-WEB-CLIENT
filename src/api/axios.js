b162f9199a74cd1b03f2fa1ea6345f1532a060c2
ocahs9 <piorag99@gmail.com>
2024-10-09 02:23:47 +0900

    feat: axios 기본 설정 구현

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,2 +1,23 @@
 import axios from "axios";
-import {useNa}
+import { useNavigate } from "react-router-dom";
+
+const getAccessToken = () => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    try {
+      //accessToken이 잘못된 형식일때를 대비
+      const token = JSON.parse(accessToken);
+      return token;
+    } catch (error) {
+      console.error("Failed to parse user from localStorage", error);
+      return "";
+    }
+  }
+};
+
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${getAccessToken()}`,
+  },
+});
--- src/pages/KakaoAuth.jsx
+++ src/pages/KakaoAuth.jsx
@@ -30,6 +30,7 @@ const KakaoAuth = () => {
     }
   };
 
+  getAccessToken();
   return <div></div>;
 };
 
